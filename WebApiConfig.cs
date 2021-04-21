5004d483e7a8a969c7149581f6202ee53621ee9a
Malik <mlk.2plus@gmail.com>
2021-04-21 13:51:33 +0200

    to handle Convention-based routing method, and i add my own route

--- WebApiConfig.cs
+++ WebApiConfig.cs
@@ -11,14 +11,33 @@ namespace DemoWebAPI
         {
             // Web API configuration and services
 
-            // Web API routes
+            // Web API routes //Attribute-based routing
             config.MapHttpAttributeRoutes();
 
+
+            // Convention-based routing
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // My own route--Mitt eget route
+            // The link will be for example: http://www.mybook.com/api/posts/archive/2019/03/21
+            // or: http://www.mybook.com/api/posts/archive/2019/03
+            // or: http://www.mybook.com/api/posts/archive/2019
+            config.Routes.MapHttpRoute(
+                name: "myRoute",
+                routeTemplate: "api/{controller}/{action}/{year}/{month}/{day}",
+                defaults: new { 
+                    month = RouteParameter.Optional,
+                    day = RouteParameter.Optional
+                },
+                constraints: new
+                {
+                    month=@"\d{0,2}", day=@"\d{0,2}"
+                }
+                ) ;
         }
     }
 }
